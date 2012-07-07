56b9bcf911b1737d8b651c13d33521b909b6fed9
Robert M Ochshorn <mail@RMOZONE.COM>
2012-07-07 20:51:15 +0545

    render routes on map

--- yatayat.js
+++ yatayat.js
@@ -16,3 +16,25 @@ YY.Segment = function(listOfLatLng, tag) {
     this.listOfLatLng = listOfLatLng;
     this.tag = tag;
 };
+
+
+
+YY.render = function(route, map) {
+    // draw a route on a map!
+
+    // render segments as polylines
+    route.segments.forEach(function(seg) {
+        var latlngs = seg.listOfLatLng.map(function(LL) {
+            return new L.LatLng(LL[0], LL[1]);
+        });
+        var poly = new L.Polyline(latlngs, {color: 'red'});
+        map.addLayer(poly);
+    });
+
+    // and stops as markers
+    route.stops.forEach(function(stop) {
+        var latlng = new L.LatLng(stop.lat, stop.lng);
+        var marker = new L.Marker(latlng);
+        map.addLayer(marker);
+    });
+};
