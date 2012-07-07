04bca62ebc3bb64fe096a9d3115069a0731700a2
Robert M Ochshorn <mail@RMOZONE.COM>
2012-07-07 21:10:05 +0545

    show route name on click

--- yatayat.js
+++ yatayat.js
@@ -4,12 +4,14 @@ YY.Route = function(stops, segments, tag) {
     this.stops = stops;
     this.segments = segments;
     this.tag = tag;
+    this.name = tag.name;
 };
 
 YY.Stop = function(lat, lng, tag) {
     this.lat = lat;
     this.lng = lng;
     this.tag = tag;
+    this.name = tag.name;
 };
 
 YY.Segment = function(listOfLatLng, tag) {
@@ -28,6 +30,7 @@ YY.render = function(route, map) {
             return new L.LatLng(LL[0], LL[1]);
         });
         var poly = new L.Polyline(latlngs, {color: 'red'});
+        poly.bindPopup(route.name);
         map.addLayer(poly);
     });
 
@@ -35,6 +38,7 @@ YY.render = function(route, map) {
     route.stops.forEach(function(stop) {
         var latlng = new L.LatLng(stop.lat, stop.lng);
         var marker = new L.Marker(latlng);
+        marker.bindPopup(stop.name);
         map.addLayer(marker);
     });
 };
