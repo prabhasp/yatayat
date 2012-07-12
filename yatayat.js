ba7043c0571da5fa35b68947b69a62f3c913fa64
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2012-07-12 16:42:44 +0545

    adding orienting routes

--- yatayat.js
+++ yatayat.js
@@ -1,30 +1,47 @@
 var YY = YY || {}
 
-YY.Route = function(stops, segments, tag) {
+YY.Route = function(stops, segments, tag, orientingSegmentID) {
     this.stops = stops;
     this.segments = segments;
     this.tag = tag;
     this.name = tag.name;
+    this.orientingSegmentID = orientingSegmentID;
     this.order();
 };
 
-YY.Route.prototype.order = function() {
+YY.Route.prototyp.order = function() {
+    var route = this;
+    
+    // find orienting way
+
+    // go through it, putting all public stops in
+
+    // find the end in another way, and continue
+    
+
+};
+
+
+YY.Route.prototype.order2 = function() {
     // order segments
 
     // order step 1: collect together ends of segments (assume ends match) 
     var segmentEnds = {};
-    var neighborlist = {};
+    var neighborDict = {};
+    var route = this;
+    
+
     console.log(this.name);
     _.each(this.segments, function(seg, idx) {
         function makeNeighbor(a, b) {
             var id, id2;
             if(a[0] < b[0]) { id = a; id2 = b; }
             else { id = b; id2 = a; }
-            if(neighborlist[id[0]] === undefined) {
-                neighborlist[id[0]] = [id2, id[1]];
+            if(neighborDict[id[0]] === undefined) {
+                neighborDict[id[0]] = [id2, id[1], seg.id];
             } else {
-                if(neighborlist[id2[0]]) throw "bad algorithm!";
-                neighborlist[id2[0]] = [id, id2[1]];
+                if(neighborDict[id2[0]]) throw "bad algorithm!";
+                neighborDict[id2[0]] = [id, id2[1], seg.id];
             }
         }
         var l1 = seg.listOfLatLng[0];
@@ -37,30 +54,50 @@ YY.Route.prototype.order = function() {
         segmentEnds[l1] = [idx, 'l'];
         segmentEnds[l2] = [idx, 'r'];
     });
-    console.log(neighborlist);
+    console.log(neighborDict);
     // order stops based on segments
-    var seed = _.keys(neighborlist)[0]; 
-    function f(route, key, flipped) {
-        var neighborArr = neighborlist[key];
+    var orientingSegmentKey, newNeighborDict;
+    _.each(neighborDict, function(v, k) {
+        if(v[2] === route.orientingSegmentID) {
+            //flipp neighborDict
+            if (v[1] == 'l' && !newNeighborDict) {    
+                console.log('flipping');
+                newNeighborDict = {}
+                _.each(neighborDict, function(v2, k2) {
+                    console.log(k2, v2);
+                    //newNeighborDict[v2[0][0]] = [k2, v2[1]], v2[0][1]];
+                });
+                neighborDict = newNeighborDict;
+            }
+            orientingSegmentKey = k;
+        }
+    });
+    console.log('orienting with ', orientingSegmentKey);
+    var started = false;
+    function f(key, flipped) {
+        if (key == orientingSegmentKey && started) return;
+        started = true;
+        var neighborArr = neighborDict[key];
         var stops = route.segments[key].orderedListofStops;
-        //if (neighborArr[1]==='l') { stops = stops.reverse(); }
+        if (flipped) { stops = stops.reverse(); }
         console.log(key, flipped);
         _.each(stops, function(x) {console.log(x.tag.name); });
         var flippedNext = (neighborArr[1] === neighborArr[0][1]) ? !flipped : flipped;
-        if (key == seed) return;
-        f(route, neighborArr[0][0], flippedNext);
+        f(neighborArr[0][0], flippedNext);
     }
-    f(this, neighborlist[seed][0][0], false);
+    f(orientingSegmentKey, false);
 }
 
-YY.Stop = function(lat, lng, tag) {
+YY.Stop = function(id, lat, lng, tag) {
+    this.id = id;
     this.lat = lat;
     this.lng = lng;
     this.tag = tag;
     this.name = tag.name;
 };
 
-YY.Segment = function(listOfLatLng, tag, orderedStops) {
+YY.Segment = function(id, listOfLatLng, tag, orderedStops) {
+    this.id = id;
     this.listOfLatLng = listOfLatLng;
     this.tag = tag;
     this.orderedListofStops = orderedStops; // intermediarily needed
@@ -95,24 +132,27 @@ YY.fromOSM = function (overpassXML) {
                 myStops.push(node);
             myNodes.push([node.lat, node.lng]);
         });
-        segments[$w.attr('id')] = new YY.Segment(myNodes, tagToObj($w.find('tag')), myStops);
+        segments[$w.attr('id')] = new YY.Segment($w.attr('id'), myNodes, tagToObj($w.find('tag')), myStops);
     });
     routes = _.map($(overpassXML).find('relation'), function(r) {
         var $r = $(r);
         var myStops = [];
         var mySegments = [];
+        var orientingSegmentID;
         _.each($r.find('member'), function(m) {
             var $m = $(m); 
             if($m.attr('type') === 'way') {
+                if ($m.attr('role') === 'orienting_way')
+                    orientingSegmentID = $m.attr('ref');
                 mySegments.push(segments[$m.attr('ref')]);
             } else if ($m.attr('type') === 'node') {
                 var n = nodes[$m.attr('ref')];
                 //if($n.find('tag')public_transportation === 'stop_position') 
                 if(n && n.lat && n.lng)
-                    myStops.push(new YY.Stop(n.lat, n.lng, n.tag));
+                    myStops.push(new YY.Stop($m.attr('ref'), n.lat, n.lng, n.tag));
             } 
         });
-        return new YY.Route(myStops, mySegments, tagToObj($r.find('tag')));
+        return new YY.Route(myStops, mySegments, tagToObj($r.find('tag')), orientingSegmentID);
     });
     return routes;
 }
