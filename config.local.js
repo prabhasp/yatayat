8aced1614d650614c424975dcc88c4e8637d8b74
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2012-07-23 07:58:35 +0545

    in case you want local development with tiles

--- config.local.js
+++ config.local.js
@@ -1,6 +1,7 @@
 var YY = YY || {};
 
 YY.API_URL = 'sample/lotsOfRoutes.xml';
+//YY.TILE_SOURCE = 'http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png';
 YY.GET_OR_POST = 'GET';
 
 YY.LNG = 85.3;
