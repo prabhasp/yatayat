bd3ae63ebf60581b76230234ccf401ac62383dfc
Robert M Ochshorn <rmo@NUMM.ORG>
2013-04-04 16:54:40 -0400

    cli_dataquality: take xml path from cli argv instead of json conf

--- cli_dataquality.js
+++ cli_dataquality.js
@@ -1,16 +1,17 @@
+#!/usr/bin/env nodejs
+
 var yy = require('./yatayat.js');
-var conf = require('./config.js');
 var DQ = require('./dataquality.js');
 
 var fs = require("fs");
 
 // Get "system" -- assumes that overpass XML is stored locally
-if(conf.GET_OR_POST === "POST") {
-    throw "cli_dataquality requires local XML data";
+if(process.argv.length !== 3) {
+    throw "cli_dataquality requires path to XML data";
 }
 
 // Load system as YY.System
-var system = yy(fs.readFileSync(conf.API_URL, "utf-8"));
+var system = yy(fs.readFileSync(process.argv[2], "utf-8"));
 
 // Run tests: this will be silent if there are no errors
 console.log(DQ.findErrors(system));
